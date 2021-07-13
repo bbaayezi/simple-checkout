c80a87fa7a8fed60206426196bfd4eb0fda6e515
bbaayezi <bbaayezi@gmail.com>
2021-07-13 03:19:13 +0100

    Update tests

--- src/com/robin/checkoutSystem/tests/ShoppingCartTest.java
+++ src/com/robin/checkoutSystem/tests/ShoppingCartTest.java
@@ -4,6 +4,7 @@ import com.robin.checkoutSystem.ShoppingCart;
 import com.robin.checkoutSystem.abstractClass.AbstractDeal;
 import com.robin.checkoutSystem.abstractClass.DealRule;
 import com.robin.checkoutSystem.deals.BuyNGetOneFreeDeal;
+import com.robin.checkoutSystem.deals.MealDeal;
 import com.robin.checkoutSystem.deals.MultiPricedDeal;
 import com.robin.checkoutSystem.interfaces.Deal;
 import org.junit.Before;
@@ -40,7 +41,6 @@ public class ShoppingCartTest {
     public void checkoutWithNoDealRule() {
         ShoppingCart cart = new ShoppingCart(skus, productPrice);
         Double finalPrice = cart.checkout();
-        System.out.println("Cart checkout price: " + finalPrice);
         assertEquals(Double.valueOf(1.35), finalPrice);
     }
 
@@ -60,12 +60,10 @@ public class ShoppingCartTest {
         cart.addItem("A", "A");
 
         Double finalPrice = cart.checkout();
-        System.out.println("Cart checkout price: " + finalPrice);
         assertEquals(Double.valueOf(2.0), finalPrice);
 
         cart.addItem("A", "A");
         finalPrice = cart.checkout();
-        System.out.println("Cart checkout price: " + finalPrice);
         assertEquals(Double.valueOf(2.65), finalPrice);
     }
 
@@ -83,7 +81,6 @@ public class ShoppingCartTest {
         cart.setDealRules(dealRules);
 
         Double finalPrice = cart.checkout();
-        System.out.println("Cart checkout price: " + finalPrice);
         assertEquals(Double.valueOf(1.35), finalPrice);
     }
 
@@ -122,5 +119,78 @@ public class ShoppingCartTest {
 
         assertEquals(Double.valueOf(1.60), cart.checkout());
         assertEquals(2, Collections.frequency(cart.getShoppingList(), "C"));
+    }
+
+    @Test
+    public void checkoutWithMealDeal() {
+        dealRules.clear();
+        rule.clear();
+        rule.put("D", 1);
+        rule.put("E", 1);
+        Double dealPrice = 3.25;
+
+        MealDeal multiPricedDeal = new MealDeal(rule, dealPrice);
+        dealRules.add(multiPricedDeal);
+
+        List<String> skus = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E"));
+        ShoppingCart cart = new ShoppingCart(skus, productPrice);
+        cart.setDealRules(dealRules);
+
+        assertEquals(Double.valueOf(4.60), cart.checkout());
+    }
+
+    @Test
+    public void checkoutWithMealDealButRequirementsNotMeet() {
+        dealRules.clear();
+        rule.clear();
+        rule.put("D", 1);
+        rule.put("E", 1);
+        Double dealPrice = 3.25;
+
+        MealDeal mealDeal = new MealDeal(rule, dealPrice);
+        dealRules.add(mealDeal);
+
+        List<String> skus = new ArrayList<>(Arrays.asList("A", "B", "C", "D"));
+        ShoppingCart cart = new ShoppingCart(skus, productPrice);
+        cart.setDealRules(dealRules);
+
+        assertEquals(Double.valueOf(2.85), cart.checkout());
+    }
+
+    @Test
+    public void checkoutWithMultipleDeals() {
+        List<AbstractDeal> dealRules = new ArrayList<>();
+
+        Map<String, Integer> rule1 = new HashMap<>();
+        rule1.put("A", 2);
+        MultiPricedDeal multiPricedDeal = new MultiPricedDeal(rule1, 0.8);
+
+        Map<String, Integer> rule2 = new HashMap<>();
+        rule2.put("D", 1);
+        rule2.put("E", 1);
+        MealDeal meadDeal = new MealDeal(rule2, 3.25);
+
+        Map<String, Integer> rule3 = new HashMap<>();
+        rule3.put("C", 3);
+        BuyNGetOneFreeDeal buyNGetOneFreeDeal = new BuyNGetOneFreeDeal(rule3, 0.75);
+
+        List<String> skus = new ArrayList<>();
+        skus.addAll(Collections.nCopies(5, "A"));
+        skus.addAll(Collections.nCopies(2, "B"));
+        skus.addAll(Collections.nCopies(6, "C"));
+        skus.addAll(Collections.nCopies(4, "D"));
+        skus.addAll(Collections.nCopies(5, "E"));
+        ShoppingCart cart = new ShoppingCart(skus, productPrice);
+
+        dealRules.add(multiPricedDeal);
+        dealRules.add(meadDeal);
+        dealRules.add(buyNGetOneFreeDeal);
+
+        cart.setDealRules(dealRules);
+
+        // (2 * 0.8 + 0.5) + (4 * 3.25 + 2.0) + 6 * 0.25
+        assertEquals(Double.valueOf(19.8), cart.checkout());
+
+        assertEquals(8, Collections.frequency(cart.getShoppingList(), "C"));
     }
 }
